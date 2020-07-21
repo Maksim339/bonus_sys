aa73075509e537d5d817de0da1f6598074298267
Maksim339 <maksimlizik6@mail.ru>
2020-07-21 17:09:53 +0800

    fix detecting markers

--- detect_markers.py
+++ detect_markers.py
@@ -5,14 +5,16 @@ import numpy as np
 
 
 
-img = cv2.imread('kbs6.jpg', cv2.IMREAD_UNCHANGED)
+
+img = cv2.imread('kbs6_turn.jpg', cv2.IMREAD_UNCHANGED)
 template = cv2.imread('template.jpg', cv2.IMREAD_UNCHANGED)
 
+# print(img)
 
 result = cv2.matchTemplate(img, template, cv2.TM_SQDIFF_NORMED)
 
 
-threshold = 0.05
+threshold = 0.95
 
 locations = np.where(result <= threshold)
 
@@ -24,13 +26,23 @@ if locations:
 
     needle_w = template.shape[1]
     needle_h = template.shape[0]
-    line_color = (0, 255, 0)
+    line_color = (200, 50, 255)
     line_type = cv2.LINE_4
 
     for loc in locations:
         top_left = loc
+
         bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
         cv2.rectangle(img, top_left, bottom_right, line_color, line_type)
+        # print(top_left)
 
+        print(bottom_right)
+#
+# print(top_left)
+# print(bottom_right)
+top = (692, 579)
+bottom = (777, 364)
+print(type(top))
+cv2.rectangle(img, top, bottom, line_color, line_type)
 
-    cv2.imwrite('test.jpg', img)
+cv2.imwrite('test.jpg', img)
