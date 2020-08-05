69fb633cae348479f709bbff17e2b6300887cb72
Maksim339 <maksimlizik6@mail.ru>
2020-08-05 10:02:50 +0800

    init

--- df.py
+++ df.py
@@ -19,7 +19,7 @@ mycursor.execute("CREATE TABLE bonussys (name VARCHAR(255), address VARCHAR(255)
 sql = "INSERT INTO bonussys (name, address, value) VALUES (%s, %s, %s)"
 val = [
   ('hundred.3.jpg', 'hundred.3.jpg', '12'),
-  ('hundred.5.jpg', 'Apple st 652', '45'),
+  ('hundred.5.jpg', 'Apple st 652', 'invalid'),
   ('hundred.6.jpg', 'Mountain 21', '100'),
   ('hundred.3.jpg', 'Valley 345', '45'),
   ('hundred.5.jpg', 'Ocean blvd 2', 'invalid'),
@@ -32,21 +32,33 @@ val = [
   ('hundred.3.jpg', 'Main Road 989', '100'),
   ('hundred.3.jpg', 'Sideway 1633', '45')
 ]
-list = []
+
 mycursor.executemany(sql, val)
-# mycursor.execute("SELECT * FROM bonussys WHERE name = 'hundred.3.jpg'")
+
 for img in fds:
+    list = []
+    list2 = []
     print(str(img))
     mycursor.execute("SELECT value, COUNT(*) FROM bonussys WHERE name = '" + img + "'GROUP BY value")
     myresult = mycursor.fetchall()
+    # print(myresult)
     count = None
     for i in range(len(myresult)):
         reg = re.compile("[),'(]")
         count = reg.sub("", str(myresult[i]))
         counter = int(count.split()[1])
+        # print(counter)
         list.append(counter)
-        print(count)
-    print(max(list))
+    for i in range(len(myresult)):
+        # print((myresult[i])[0])
+        list2.append((myresult[i])[0])
+        print(list2)
+    a = list.index(max(list))
+    print(list2[a])
+
+    sql = "INSERT INTO verified (filename, value) VALUES (%s, %s)"
+    val = (str(img), str(list2[a]))
+    mycursor.execute(sql, val)
 
 mydb.commit()
 
