db0b16791b16c8b2ddb152a07cb650ef4ad67e52
Maksim339 <maksimlizik6@mail.ru>
2020-08-05 18:24:34 +0800

    сборка

--- df.py
+++ df.py
@@ -2,63 +2,51 @@ import mysql.connector
 import re
 import pathlib
 import os
-path = pathlib.Path('fff').absolute()
-fds = os.listdir(path)
 
+path = pathlib.Path('result').absolute()
+fds = os.listdir(path)
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="9f7S39D5!",
-    database="mydatabase"
+    database="mydatabase",
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("DROP TABLE bonussys")
-mycursor.execute("CREATE TABLE bonussys (name VARCHAR(255), address VARCHAR(255), value VARCHAR(7))")
-sql = "INSERT INTO bonussys (name, address, value) VALUES (%s, %s, %s)"
-val = [
-  ('hundred.3.jpg', 'hundred.3.jpg', '12'),
-  ('hundred.5.jpg', 'Apple st 652', 'invalid'),
-  ('hundred.6.jpg', 'Mountain 21', '100'),
-  ('hundred.3.jpg', 'Valley 345', '45'),
-  ('hundred.5.jpg', 'Ocean blvd 2', 'invalid'),
-  ('hundred.6.jpg', 'Green Grass 1', '45'),
-  ('hundred.3.jpg', 'Sky st 331', '11'),
-  ('hundred.3.jpg', 'One way 98', '11'),
-  ('hundred.3.jpg', 'Yellow Garden 2', '45'),
-  ('hundred.8.jpg', 'Park Lane 38', '100'),
-  ('hundred.3.jpg', 'Central st 954' , '100'),
-  ('hundred.3.jpg', 'Main Road 989', '100'),
-  ('hundred.3.jpg', 'Sideway 1633', '45')
-]
+# #
+# mycursor.execute("CREATE TABLE users (user_id VARCHAR(255), file_id VARCHAR(255))")
+# mycursor.execute("CREATE TABLE results (user_id VARCHAR(255), file_id VARCHAR(255), value VARCHAR(10))")
+# mycursor.execute("CREATE TABLE verified (user_id VARCHAR(255), file_id VARCHAR(255), value VARCHAR(10))")
 
-mycursor.executemany(sql, val)
+def del_mac():
+    if os.path.exists(path + ".DS_Store"):
+        os.remove(path + ".DS_Store")
 
-for img in fds:
-    list = []
-    list2 = []
-    print(str(img))
-    mycursor.execute("SELECT value, COUNT(*) FROM bonussys WHERE name = '" + img + "'GROUP BY value")
-    myresult = mycursor.fetchall()
-    # print(myresult)
-    count = None
-    for i in range(len(myresult)):
-        reg = re.compile("[),'(]")
-        count = reg.sub("", str(myresult[i]))
-        counter = int(count.split()[1])
-        # print(counter)
-        list.append(counter)
-    for i in range(len(myresult)):
-        # print((myresult[i])[0])
-        list2.append((myresult[i])[0])
-        print(list2)
-    a = list.index(max(list))
-    print(list2[a])
 
-    sql = "INSERT INTO verified (filename, value) VALUES (%s, %s)"
-    val = (str(img), str(list2[a]))
-    mycursor.execute(sql, val)
+def insert_verified():
+    for img in fds:
+        print('fsdfsd')
+        list = []
+        list2 = []
+        img = img.replace('.jpg', '')
+        print(str(img))
+        mycursor.execute("SELECT value, COUNT(*) FROM results WHERE file_id = '" + img + "'GROUP BY value")
+        myresult = mycursor.fetchall()
+        for i in range(len(myresult)):
+            reg = re.compile("[),'(]")
+            count = reg.sub("", str(myresult[i]))
+            counter = int(count.split()[1])
+            list.append(counter)
+        for i in range(len(myresult)):
+            list2.append((myresult[i])[0])
+            print(list2)
+        a = list.index(max(list))
+        print(list2[a])
 
-mydb.commit()
+        sql = "INSERT INTO verified (filename, value) VALUES (%s, %s)"
+        val = (str(img), str(list2[a]))
+        mycursor.execute(sql, val)
+
+    mydb.commit()
 
---
+++ main.py
@@ -0,0 +1,150 @@
+import mysql.connector
+import os
+import df
+import pathlib
+import shutil
+import re
+
+path = r'/home/maksim/PycharmProjects/bonus_sys/for_bonus/'
+resultpath = r'/home/maksim/PycharmProjects/bonus_sys/result/'
+fds = os.listdir(resultpath)
+print(fds)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="9f7S39D5!",
+    database="mydatabase",
+    port='3306'
+)
+
+mycursor = mydb.cursor()
+
+#
+# def del_mac():
+#     if os.path.exists(path + ".DS_Store"):
+#         os.remove(path + ".DS_Store")
+
+
+# # --СКОЛЬКО РАЗ БЫЛО ОТРАВЛЕННО ИЗОБРАЖЕНИЕ.
+
+def remove_file(file_id):
+    files = os.listdir(path)
+    mycursor.execute("SELECT file_id, COUNT(*) FROM users GROUP BY file_id")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        count = x[-1]
+        if count > 2:
+            for img in fds:
+                print('fsdfsd')
+                list = []
+                list2 = []
+                img = img.replace('.jpg', '')
+                print(str(img))
+                mycursor.execute("SELECT value, COUNT(*) FROM results WHERE file_id = '" + img + "'GROUP BY value")
+                myresult = mycursor.fetchall()
+                for i in range(len(myresult)):
+                    reg = re.compile("[),'(]")
+                    count = reg.sub("", str(myresult[i]))
+                    counter = int(count.split()[1])
+                    list.append(counter)
+                for i in range(len(myresult)):
+                    list2.append((myresult[i])[0])
+                    print(list2)
+                a = list.index(max(list))
+                print(list2[a])
+
+                sql = "INSERT INTO verified (filename, value) VALUES (%s, %s)"
+                print('zopa')
+                val = (str(img), str(list2[a]))
+                mycursor.execute(sql, val)
+            filename = str(file_id) + '.jpg'
+            shutil.copyfile(path + filename, resultpath + filename)
+            # if os.path.exists(path + filename):
+            os.remove(path + filename)
+            sql = "DELETE FROM users WHERE file_id = '{}'".format(file_id)
+            mycursor.execute(sql)
+            mydb.commit()
+            print(mycursor.rowcount, "record(s) deleted")
+
+            # print(count)
+
+
+def remove_bd(file_id):
+    # remove_file(file_id)
+    mycursor.execute("SELECT file_id, COUNT(*) FROM users GROUP BY file_id")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        count = x[-1]
+        if count > 1:
+            sql = "DELETE FROM users WHERE file_id = '{}'".format(file_id)
+            mycursor.execute(sql)
+            mydb.commit()
+            print(mycursor.rowcount, "record(s) deleted")
+
+
+# remove_bd(65)
+
+def insert_sql(user_id, file_id):
+    sql = "INSERT INTO users (user_id, file_id) VALUES (%s, %s)"
+    val = (user_id, file_id)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
+
+def validation(user_id, file_id):
+    mycursor.execute("SELECT * FROM users WHERE user_id = '{}' AND file_id = '{}'".format(user_id, file_id))
+    myresult = mycursor.fetchone()
+    # print(myresult)
+    return myresult
+
+
+# validation(949191326, 188)
+# #
+# sql = "DELETE FROM users WHERE user_id = '604652371'"
+# mycursor.execute(sql)
+# mydb.commit()
+# print(mycursor.rowcount, "record(s) deleted")
+
+
+def check(user_id, file_id):
+    files = os.listdir(path)
+    if os.path.exists(path + ".DS_Store"):
+        os.remove(path + ".DS_Store")
+    bd_valid = validation(user_id, file_id=file_id)  # Проверка, есть ли айди юзера и фото в БД
+    num = 0
+    while True:
+        if bd_valid is None:  # НЕ НАШЕЛ В БАЗЕ!
+            if os.path.exists(path + ".DS_Store"):
+                os.remove(path + ".DS_Store")
+            file_id = file_id.split('.jpg')[0]
+            return 'ok', file_id
+
+        elif bd_valid:  # НАШЕЛ В БАЗЕ!
+            if os.path.exists(path + ".DS_Store"):
+                os.remove(path + ".DS_Store")
+            try:
+                file = files[num]
+                file_id = file.split('.jpg')[0]
+                bd = validation(user_id, file_id=file_id)
+                if bd is None:
+                    return 'ok', file_id
+                else:
+                    num += 1
+            except IndexError:
+                return 'finish'
+
+
+# check(222, 188)
+
+# validation(222,188)
+
+def remove5(file_id):
+    files = os.listdir(path)
+    # mycursor.execute("SELECT file_id, COUNT(*) as count FROM users GROUP BY file_id")
+    mycursor.execute("SELECT file_id, COUNT(*) FROM users WHERE file_id = '{}'".format(file_id))
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+
+# remove5('05571edda43fba70df828cd197b10a6f')
